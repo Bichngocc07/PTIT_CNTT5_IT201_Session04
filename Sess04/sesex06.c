84aae94a366fcf86da39a5063eb95d92ad82dcac
Bichngocc07 <ngoc01072006@gmail.com>
2025-06-25 15:50:13 +0700

    Update sesex06.c

--- Sess04/sesex06.c
+++ Sess04/sesex06.c
@@ -2,26 +2,51 @@
 // Created by Owner on 6/25/2025.
 //
 #include <stdio.h>
+#include <string.h>
+
+#define SIZE 5
+
+typedef struct {
+    int id;
+    char name[50];
+    int age;
+} Student;
 
 int main() {
-    int n ,i ;
-    printf("Nhap so luong phan tu: ");
-    scanf("%d", &n);
-    int arr[n];
-    for (i = 0; i < n; i++) {
-        printf("Nhap phan tu: ");
-        scanf("%d", &arr[i]);
+    Student students[SIZE];
+    int searchId, found = 0;
+
+    //thông tin sinh viên
+    printf("Nhap thong tin cho %d sinh vien:\n", SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        printf("\nSinh vien %d:\n", i + 1);
+        printf("ID: ");
+        scanf("%d", &students[i].id);
+        getchar(); // Đọc ký tự newline sau scanf
+        printf("Ten: ");
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        students[i].name[strcspn(students[i].name, "\n")] = '\0'; // Xoá dấu newline
+        printf("Tuoi: ");
+        scanf("%d", &students[i].age);
     }
-    int max = -1 ;
-    int check;
-    printf("Phan tu: ");
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-            check = i;
+
+    // Nhập id 
+    printf("\nNhap ID can tim: ");
+    scanf("%d", &searchId);
+
+    for (int i = 0; i < SIZE; i++) {
+        if (students[i].id == searchId) {
+            printf("\nSinh vien tim thay:\n");
+            printf("ID: %d\nTen: %s\nTuoi: %d\n", students[i].id, students[i].name, students[i].age);
+            found = 1;
+            break;
         }
-        if (arr[i] == max) {
-            printf("Phan tu: ");
-        }}
-    return 0;
     }
+
+    if (!found) {
+        printf("\nSinh vien khong ton tai\n");
+    }
+
+    return 0;
+}
+
